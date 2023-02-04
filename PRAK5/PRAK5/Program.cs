2e99fa32089f2c94ce90eb86a4d0daaff1b91cfc
Danon374 <114215757+Danon374@users.noreply.github.com>
2023-02-04 19:46:24 +0300

    Add files via upload

---
+++ PRAK5/PRAK5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PRAK5
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Zakaz menu = new Zakaz();
+            menu.MENU();
+        }
+    }
+}
